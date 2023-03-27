20d242bb55ba2581ca55cfbf352bc8ce465f9e0c
Ligia Pretel Eimantas <ligia.pretel@yahoo.com.br>
2023-03-26 21:46:10 -0300

    Aula 01 - Instalação React e configurações iniciais

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
+
+  function handleSubmit(event){
+    event.preventDefault();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />        
+      <main>
+        <form onSubmit={handleSubmit}>
+          <label>Nome</label>
+          <input type="text"/>
+
+          <label>E-mail</label>
+          <input type="email"/>
+
+          <label>Senha</label>
+          <input type="password"/>  
+          
+          <button type="submit">Enviar</button> 
+        </form>
+      </main>
+    </>
   );
 }
 
